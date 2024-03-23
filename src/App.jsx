3e66ffe4644aaf63c706b7c4b26187b5f1933c9e
mumin6 <am.mumin99@outlook.com>
2024-03-23 21:32:30 +0600

    added banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './components/Banner/Banner'
 import Header from './components/header/Header'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     
       <Header></Header>
+      <Banner></Banner>
       
     </>
   )
