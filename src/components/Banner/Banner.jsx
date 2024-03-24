e9f48cd4c730e0fc52426fd1494b75584892f7c3
mumin6 <am.mumin99@outlook.com>
2024-03-24 09:41:52 +0600

    added json file

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,8 +9,8 @@ const Banner = () => {
                         <h1 className="mb-5 text-4xl font-bold">Discover an exceptional cooking class tailored for you!</h1>
                         <p className="mb-5">
                             Embark on a culinary adventure with Flavor Fusion, your ultimate destination for diverse recipes, cooking tips, and culinary inspiration.</p>
-                        <button className="btn btn-success">Explore Now</button>
-                        <button className="btn glass lg:ml-4">Our Feedback</button>
+                        <button className="btn rounded-3xl btn-success">Explore Now</button>
+                        <button className="btn rounded-3xl glass lg:ml-4">Our Feedback</button>
                     </div>
                 </div>
             </div>
--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -2,7 +2,7 @@
 
 const Recipes = () => {
     return (
-        <div className="text-center mt-8 max-w-screen-md mx-auto">
+        <div className="text-center mt-8 lg:max-w-screen-md mx-auto">
             <h1 className="text-4xl font-semibold">Our Recipes</h1>
             <p className="mt-4 font-normal">Indulge in the vibrant flavors and colors of Asia with our tantalizing Grilled Vegetable Platter recipe. Perfect for a cozy dinner at home, this dish showcases the natural sweetness of seasonal vegetables infused with aromatic herbs and tangy citrus.</p>
         </div>
