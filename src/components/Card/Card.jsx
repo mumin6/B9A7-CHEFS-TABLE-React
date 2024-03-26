7b552c41151bb99f03987951c7a12582b659cb8f
mumin6 <am.mumin99@outlook.com>
2024-03-26 16:02:32 +0600

    minor fixes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,19 +5,19 @@ import { FaFire } from "react-icons/fa";
 const Card = ({ card }) => {
 
     const { recipe_image, recipe_name, short_description, ingredients, preparing_time, calories } = card;
-    const name = ingredients.map(ig => <li className="text-neutral-500">{ig}</li>)
+    const name = ingredients.map(ig => <li className="text-neutral-500">{ig}</li>);
 
     return (
-        <div className="m-2 p-2 bg-slate-200 rounded-2xl">
-            <img className="rounded-2xl" src={recipe_image} alt="" srcset="" />
+        <div className="m-2 p-2 bg-zinc-200 rounded-2xl">
+            <img className="rounded-2xl" src={recipe_image} alt="" />
             <h1 className=" font-semibold text-xl text-black mt-2">{recipe_name}</h1>
             <p className="text-neutral-500">{short_description}</p>
             <p className="text-lg text-black">Ingredients: {ingredients.length}</p>
             <ul className="mt-2">{name}</ul>
-            <div className="mt-4 flex align-middle">
-                <p className="mx-2 flex text-center text-black"> <LuClock3/> <span className="ml-2">{preparing_time}</span>minutes</p>
+            <div className="mt-4 flex  items-center">
+                <p className="mx-2 flex  items-center text-black"> <LuClock3/> <span className="ml-2">{preparing_time}</span>minutes</p>
 
-                <p className="ml-4 flex text-center"><FaFire /> <span className="ml-2">{calories} calories</span></p>
+                <p className="ml-4 flex items-center"><FaFire /> <span className="ml-2">{calories} calories</span></p>
             </div>
             <button className="btn rounded-3xl btn-success text-center mt-4">Want to Cook</button>
         </div>
--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -4,7 +4,7 @@ const Recipes = () => {
     return (
         <div className="text-center mt-8 lg:max-w-screen-md mx-auto">
             <h1 className="text-4xl font-semibold">Our Recipes</h1>
-            <p className="mt-4 font-normal">Indulge in the vibrant flavors and colors of Asia with our recipe. Perfect for a cozy dinner at home, this dishes showcases the natural sweetness of seasonal vegetables infused with aromatic herbs and tangy citrus.</p>
+            <p className="mt-4 font-normal text-neutral-600">Indulge in the vibrant flavors and colors of Asia with our recipe. Perfect for a cozy dinner at home, this dishes showcases the natural sweetness of seasonal vegetables infused with aromatic herbs and tangy citrus.</p>
         </div>
     );
 };
